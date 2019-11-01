2451e2afc82a7c7f220bc76c861644ab5e4ff895
NikosKyriakakis <50583666+NikosKyriakakis@users.noreply.github.com>
2019-11-01 20:07:49 +0200

    Update parser.h

--- KMeans/include/parser.h
+++ KMeans/include/parser.h
@@ -10,10 +10,6 @@
 #define TITLE 15
 #define MAX_CLUSTERS 5
 
-static void get_string_tokens(char *string, char **tokens);
-static char **allocate_tokens();
-static float get_max(const float a, const float b);
-static float get_min(const float a, const float b);
 extern int read_cfg_file(config_t *cfg, const char *filename);
 extern int set_cluster_num(const config_t *cfg, int *clusters);
 extern int read_data_file(list_t *point_list, axis_t *axis, const char *filename);
